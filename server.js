0e8e807a1ac53adf6a2af754c8b39d5848ab7dd3
abhirudramaniyadav7-svg <abhirudramaniyadav7@gmail.com>
2026-08-23 10:06:12 +0530

    Refactor server.js for clarity and organization
    Updated server.js to improve code structure and comments.

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname));
+// Serve all website files
+app.use(express.static(path.join(__dirname)));
 
+// Homepage
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Spotify callback
 app.get("/callback", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+// Start server
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Bollywood Safar running on port ${PORT}`);
 });
